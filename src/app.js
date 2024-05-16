4b3563363f6b9d6a09e943a422448a52a620b6ed
Jetrca92 <blaz.guzej@gmail.com>
2024-05-16 20:35:43 +0200

    add mongodb, dotenv

--- src/app.js
+++ src/app.js
@@ -1,14 +1,42 @@
 const express = require('express');
-const app = express();
-const PORT = 3000;
+const mongoose = require('mongoose');
+const doetnv = require('dotenv');
 
+const app = express();
+mongoose.set('strictQuery', false);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+if(process.env.NODE_ENV !== 'production') {
+    doetnv.config();
+}
+const PORT = process.env.PORT || 3000;
+const CONNECTION = process.env.CONNECTION
 app.get('/', (req, res) => {
     res.send('Hello world!');
 });
 
-app.post('/', (req, res) => {
-    res.send('This is a post request');
+app.get('/api/customers', (req, res) => {
+    res.send({"customers": customers});
 })
-app.listen(PORT, () => {
-    console.log('App listening on port ' + PORT);
-})
+
+app.post('/api/customers', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
+
+const start = async() => {
+    try{
+        await mongoose.connect(CONNECTION);
+    
+        app.listen(PORT, () => {
+            console.log('App listening on port ' + PORT);
+        });
+    } catch(e) {
+        console.log(e.message)
+    }
+    
+};
+
+start();
