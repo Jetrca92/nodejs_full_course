64a26f7c4d9c750b2168d7731bcafd6db26b7c66
Jetrca92 <blaz.guzej@gmail.com>
2024-05-16 06:42:49 +0200

    Start work

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+});
+
+app.post('/', (req, res) => {
+    res.send('This is a post request');
+})
+app.listen(PORT, () => {
+    console.log('App listening on port ' + PORT);
+})
